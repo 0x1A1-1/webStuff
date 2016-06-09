6a26722a90746ac531cfbd4a2540b4fb064613f4
Cigarent <xiaojunh1996@hotmail.com>
2016-06-09 15:28:19 -0500

    Create clearEmptyMembership.php

---
+++ php/clearEmptyMembership.php
@@ -0,0 +1,23 @@
+function clearEmptyMembership(){
+		$userMemQuery = makeSQLQuery("SELECT user, membership FROM security_user_membership");
+		while ($row = $userMemQuery ->fetch(PDO::FETCH_BOTH)) {
+
+			//record correct user membership status, counts multiple
+			if($membership[$row["user"]]==""){
+				$membership[$row["user"]]=$row["membership"];
+			}else{
+				$membership[$row["user"]].=",".$row["membership"];
+			}
+		}
+
+		$secUserMemQuery = makeSQLQuery("SELECT ID, Memberships FROM security_users");
+		while ($row = $secUserMemQuery ->fetch(PDO::FETCH_BOTH)) {
+			$currentUserStatus[$row["ID"]]=$row["Memberships"];
+		}
+
+		foreach ($membership as $key => $value) {
+			if(!array_key_exists($key, $currentUserStatus)){
+				makeSQLQuery("DELETE FROM security_user_membership WHERE user='$key'");
+			}
+		}
+	}

9e32194603d947d340b703f09fcdbf8099885263
Cigarent <xiaojunh1996@hotmail.com>
2016-06-16 23:36:14 -0500

    first Async I/O nodejs

---
+++ javascript/learnyounode/program.js
@@ -0,0 +1,24 @@
+//1.
+//console.log("HELLO WORLD");
+
+// 2.
+// var sum = 0;
+// for (var i=2; i<process.argv.length; i++){
+//   sum += Number(process.argv[i]);
+// };
+// console.log(sum);
+
+// 3. Lines of file
+// var fs = require('fs');
+// var fileBuf = fs.readFileSync(process.argv[2]);
+// var lines = contents.toString().split('\n').length - 1 ;
+// console.log(lines);
+
+// 4. Lines of file async
+// var fs = require('fs')
+// var file = process.argv[2]
+// fs.readFile(file, function (err, contents) {
+//   // fs.readFile(file, 'utf8', callback) can also be used
+//   var lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
